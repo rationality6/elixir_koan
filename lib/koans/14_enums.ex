dc00734106ddaff13d1d8359b75f83b383879584
Hyun Ahn <codepawn@icloud.com>
2023-04-08 17:58:50 +0900

    wip

--- lib/koans/14_enums.ex
+++ lib/koans/14_enums.ex
@@ -40,46 +40,46 @@ defmodule Enums do
   def odd?(n), do: rem(n, 2) == 1
 
   koan "Filter allows you to only keep what you really care about" do
-    assert Enum.filter([1, 2, 3], &odd?/1) == ___
+    assert Enum.filter([1, 2, 3], &odd?/1) == [1, 3]
   end
 
   koan "Reject will help you throw out unwanted cruft" do
-    assert Enum.reject([1, 2, 3], &odd?/1) == ___
+    assert Enum.reject([1, 2, 3], &odd?/1) == [2]
   end
 
   koan "You three there, follow me!" do
-    assert Enum.take([1, 2, 3, 4, 5], 3) == ___
+    assert Enum.take([1, 2, 3, 4, 5], 3) == [1,2,3]
   end
 
   koan "You can ask for a lot, but Enum won't hand you more than you give" do
-    assert Enum.take([1, 2, 3, 4, 5], 10) == ___
+    assert Enum.take([1, 2, 3, 4, 5], 10) == [1, 2, 3, 4, 5]
   end
 
   koan "Just like taking, you can also drop elements" do
-    assert Enum.drop([-1, 0, 1, 2, 3], 2) == ___
+    assert Enum.drop([-1, 0, 1, 2, 3], 2) == [1,2,3]
   end
 
   koan "Zip-up in pairs!" do
     letters = [:a, :b, :c]
     numbers = [1, 2, 3]
-    assert Enum.zip(letters, numbers) == ___
+    assert Enum.zip(letters, numbers) == [a: 1, b: 2, c: 3]
   end
 
   koan "When you want to find that one pesky element" do
-    assert Enum.find([1, 2, 3, 4], &even?/1) == ___
+    assert Enum.find([1, 2, 3, 4], &even?/1) == 2
   end
 
   def divisible_by_five?(n), do: rem(n, 5) == 0
 
   koan "...but you don't quite find it..." do
-    assert Enum.find([1, 2, 3], &divisible_by_five?/1) == ___
+    assert Enum.find([1, 2, 3], &divisible_by_five?/1) == nil
   end
 
   koan "...you can settle for a consolation prize" do
-    assert Enum.find([1, 2, 3], :no_such_element, &divisible_by_five?/1) == ___
+    assert Enum.find([1, 2, 3], :no_such_element, &divisible_by_five?/1) == :no_such_element
   end
 
   koan "Collapse an entire list of elements down to a single one by repeating a function." do
-    assert Enum.reduce([1, 2, 3], 0, fn element, accumulator -> element + accumulator end) == ___
+    assert Enum.reduce([1, 2, 3], 0, fn element, accumulator -> element + accumulator end) == 6
   end
 end
